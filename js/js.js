4c28bb12879769a85f1adc2b55218e0b87510ca0
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2022-10-29 14:01:56 +0300

    güncelleme 0.2

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+  }
