0f33f43004de6d1dee219e59970952f7e8356b54
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2022-11-04 21:48:43 +0300

    güncelleme

--- js/js.js
+++ js/js.js
@@ -1,16 +1,8 @@
-
-
-
-
-
-
-/*
-function openCity(cityName) {
-    var i;
-    var x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    document.getElementById(cityName).style.display = "block";  
+function openCity(yolName) {
+  var i;
+  var x = document.getElementsByClassName("yol");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(yolName).style.display = "block";
 }
-*/
